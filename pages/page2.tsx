d33ae2bbe2e63e59b6e70c5fb3ad80b161f2aba5
MMel25 <maormelamed@gmail.com>
2025-08-14 22:43:30 +0300

    Create page2.tsx

---
+++ pages/page2.tsx
@@ -0,0 +1,11 @@
+import StoryPage from '../components/StoryPage';
+
+export default function Page2() {
+  return (
+    <StoryPage
+      title="עמוד 2: ההרפתקה"
+      text="יואב יצא למסע מרתק ביער הקסום, שם פגש חיות מדברות..."
+      image="/images/page2.png"
+    />
+  );
+}
