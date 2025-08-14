669eeef9e847f5d4a9482bf6f7e7e418f7748899
MMel25 <maormelamed@gmail.com>
2025-08-14 22:42:03 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../styles/globals.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>ברוכים הבאים לספר הילדים</h1>
+      <Image src="/images/cover.png" alt="כריכה" width={400} height={300} />
+      <nav>
+        <Link href="/page1">התחל לקרוא</Link>
+      </nav>
+    </div>
+  );
+}
