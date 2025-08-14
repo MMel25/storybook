fa984a5bdaccf425afd2268c9d0c0c9bc35c35a6
MMel25 <maormelamed@gmail.com>
2025-08-14 22:42:59 +0300

    Create page1.tsx

---
+++ pages/page1.tsx
@@ -0,0 +1,11 @@
+import StoryPage from '../components/StoryPage';
+
+export default function Page1() {
+  return (
+    <StoryPage
+      title="עמוד 1: ההתחלה"
+      text="פעם אחת, בארץ רחוקה, חי לו ילד קטן בשם יואב..."
+      image="/images/page1.png"
+    />
+  );
+}
