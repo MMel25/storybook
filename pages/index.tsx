f3078195c958dc42af997016b13e83f1e497e584
MMel25 <maormelamed@gmail.com>
2025-08-15 00:57:07 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/globals.module.css';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>ברוכים הבאים לספר הילדים</h1>
+      <h1>הַפִּיל שֶׁאָהַב לְנַגֵּן</h1>
       <Image src="/images/cover.png" alt="כריכה" width={400} height={300} />
       <nav>
         <Link href="/page1">התחל לקרוא</Link>
