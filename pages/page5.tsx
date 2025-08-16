fcd2377b38ade011089ed0c650a23632f99a717b
MMel25 <maormelamed@gmail.com>
2025-08-16 19:22:41 +0300

    Update page5.tsx

--- pages/page5.tsx
+++ pages/page5.tsx
@@ -119,7 +119,7 @@ export default function Page5() {
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
               viewBox="0 0 24 24" fill="none" stroke="currentColor"
               strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M3 9L12 2l9 7v11a2 2 0 0 1-2 2h-4a2 2 0r0 1-2-2V12H9v8a2 2 0 0 1-2 2H3z" />
+              <path d="M3 9L12 2l9 7v11a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V12H9v8a2 2 0 0 1-2 2H3z" />
             </svg>
           </Link>
 
